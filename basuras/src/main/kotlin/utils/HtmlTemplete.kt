b3688fad023eef20c883f3b14e0d818020e621c3
Rocio <rpalaofernandez@gmail.com>
2022-10-05 20:47:36 +0200

    html, css resoruces

--- basuras/src/main/kotlin/utils/HtmlTemplete.kt
+++ basuras/src/main/kotlin/utils/HtmlTemplete.kt
@@ -27,44 +27,57 @@ class HtmlTemplete(
                 <meta charset="UTF-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="./resumencss">
+                <link rel="stylesheet" href="./css/styleResumen.css">
                 <title>Resumen</title>
             </head>
             <body>
                 <h1>Resumen de recogidas de basura y reciclaje de $distrito</h1>
-                <h6>$fechaHora</h6>
-                <h5>Rocío Palao & Mohamed Asidah</h5>
-                <br>
+                <img id ="logo" src="./img/logo.png">
+                <p class="datos">$fechaHora</p>
+                <p class="datos">Rocío Palao & Mohamed Asidah</p>
+                
+                <hr>
+                
                 <div id="container">
-                    <h5>Número de contenedores de cada tipo que hay en cada distrito: </h5>
-                    <p>$numeroContenedoresTipoDistrito</p>
+                    <h4>Número de contenedores de cada tipo que hay en cada distrito: </h4>
+                    <p class="resultados">$numeroContenedoresTipoDistrito</p>
                     
-                    <h5>Media de contenedores de cada tipo que hay en cada distrito: </h5>
-                    <p>$mediaContenedoresTipoDistrito</p>
+                    <h4>Media de contenedores de cada tipo que hay en cada distrito: </h4>
+                    <p class="resultados">$mediaContenedoresTipoDistrito</p>
 
                     <br>
 
                     <h4>Gráfico con el total de contenedores por distrito:</h4>
                     <img src="./img/01-totalContenedoresDistrito.png">
 
-                    <h5>Media de toneladas anuales de recogidas por cada tipo de basura agrupadas por
-                    distrito:</h5>
-                    <p>$mediaToneladasAnuales/p>
+                    <br>
+
+                    <h4>Media de toneladas anuales de recogidas por cada tipo de basura agrupadas por
+                    distrito:</h4>
+                    <p class="resultados">$mediaToneladasAnuales/p>
 
                     <br>
 
                     <h4>Gráfico de media de toneladas mensuales de recogida de basura por distrito:</h4>
                     <img src="./img/02-mediaToneladasMensuales.png">
 
-                    <h5>Máximo, mínimo , media y desviación de toneladas anuales de recogidas por cada tipo
-                    de basura agrupadas por distrito:</h5>
-                    <p>$maxMinMediaDesv</p>
+                    <br>
 
-                    <h5>Suma de todo lo recogido en un año por distrito:</h5>
-                    <p>$sumaRecogidoDistrito</p>
+                    <h4>Máximo, mínimo , media y desviación de toneladas anuales de recogidas por cada tipo
+                    de basura agrupadas por distrito:</h4>
+                    <p class="resultados">$maxMinMediaDesv</p>
 
-                    <h5>Por cada distrito obtener para cada tipo de residuo la cantidad recogida:</h5>
-                    <p>$porDistritoTipoResiduoCantidad</p>
+                    <br>
+
+                    <h4>Suma de todo lo recogido en un año por distrito:</h4>
+                    <p class="resultados">$sumaRecogidoDistrito</p>
+
+                    <br>
+
+                    <h4>Por cada distrito obtener para cada tipo de residuo la cantidad recogida:</h4>
+                    <p class="resultados">$porDistritoTipoResiduoCantidad</p>
+
+                    <br>
 
                     <p>Tiempo de generación del mismo en milisegundos: <b>$tiempoGeneracion</b></p>
                 </div>
@@ -81,44 +94,73 @@ class HtmlTemplete(
         fun generateHtmlResumenDistrito(): String {
             return """
             <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="./resumencss">
-                <title>Resumen</title>
-            </head>
-            <body>
-                <h1>Resumen de recogidas de basura y reciclaje de $distrito</h1>
-                <h6>$fechaHora</h6>
-                <h5>Rocío Palao & Mohamed Asidah</h5>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="./css/styleResumen.css">
+            <title>Resumen$distrito</title>
+        </head>
+        <body>
+            <h1 id="titulo">Resumen de recogidas de basura y reciclaje de $distrito</h1>
+            <img id ="logo" src="./img/logo.png">
+            <p class="datos">$fechaHora</p>
+            <p class="datos">Rocío Palao & Mohamed Asidah</p>
+        
+            <hr>
+        
+            <div id="container">
+        
+                <h4>Número de contenedores de cada tipo que hay en cada distrito: </h4>
+                <p class="resultados">$numeroContenedoresTipoDistrito</p>
+        
                 <br>
-                <div id="container">
-                    <h5>Número de contenedores de cada tipo que hay en este distrito: </h5>
-                    <p>$numeroContenedoresTipoDistrito</p>
-
-                    <h5>Total de toneladas recogidas en ese distrito por residuo:</h5>
-                    <p>$totalToneladasResiduo</p>
-
-                    <br>
-
-                    <h4>Gráfico con el total de toneladas por residuo en ese distrito:</h4>
-                    <img src="./img/01-totalToneladasResiduo$distrito.png">
-
-                    <h5>Máximo, mínimo , media y desviación por mes por residuo en dicho distrito:</h5>
-                    <p>$maxMinMediaDesv</p>
-
-                    <br>
-
-                    <h4>Gráfica del máximo, mínimo y media por meses en dicho distrito:</h4>
-                    <img src="./img/02-calculosMeses$distrito.png">
-
-                    <p>Tiempo de generación del mismo en milisegundos: <b>$tiempoGeneracion</b></p>
-                </div>
                 
-            </body>
-            </html>
+                <h4>Media de contenedores de cada tipo que hay en cada distrito: </h4>
+                <p class="resultados">$mediaContenedoresTipoDistrito</p>
+        
+                <br>
+        
+                <h4>Gráfico con el total de contenedores por distrito:</h4>
+                <img src="./img/01-totalContenedoresDistrito.png">
+        
+                <br>
+        
+                <h4>Media de toneladas anuales de recogidas por cada tipo de basura agrupadas por
+                distrito:</h4>
+                <p class="resultados">$mediaToneladasAnuales</p>
+        
+                <br>
+        
+                <h4>Gráfico de media de toneladas mensuales de recogida de basura por distrito:</h4>
+                <img src="./img/02-mediaToneladasMensuales.png">
+        
+                <br>
+        
+                <h4>Máximo, mínimo , media y desviación de toneladas anuales de recogidas por cada tipo
+                de basura agrupadas por distrito:</h4>
+                <p class="resultados">$maxMinMediaDesv</p>
+        
+                <br>
+        
+                <h4>Suma de todo lo recogido en un año por distrito:</h4>
+                <p class="resultados">$sumaRecogidoDistrito</p>
+        
+                <br>
+        
+                <h4>Por cada distrito obtener para cada tipo de residuo la cantidad recogida:</h4>
+                <p class="resultados">$porDistritoTipoResiduoCantidad</p>
+        
+                <br>
+        
+                <p id="tiempo">Tiempo de generación del mismo en milisegundos: <b>$tiempoGeneracion</b></p>
+                
+        
+            </div>
+            
+        </body>
+        </html>
         """.trimIndent()
         }
     }

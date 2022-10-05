54fcee0efbad2b8e3277ec8d655cf1dd8791f918
Rocio <rpalaofernandez@gmail.com>
2022-10-05 19:43:18 +0200

    xml residuos listo

--- basuras/src/main/kotlin/dto/ResiduosDto.kt
+++ basuras/src/main/kotlin/dto/ResiduosDto.kt
@@ -1,15 +1,23 @@
 package dto
 
 import kotlinx.serialization.Serializable
+import nl.adaptivity.xmlutil.serialization.XmlElement
 
 @Serializable
 data class ResiduosDto(
+    @XmlElement(true)
     val año: Short,
+    @XmlElement(true)
     val mes: String,
+    @XmlElement(true)
     val lote: Byte,
+    @XmlElement(true)
     val residuo: String,
+    @XmlElement(true)
     val distrito: Byte,
+    @XmlElement(true)
     val nombreDistrito: String,
+    @XmlElement(true)
     val toneladas: Float
 ) {
 
